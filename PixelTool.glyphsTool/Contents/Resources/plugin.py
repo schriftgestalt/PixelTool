8fa1f412777977bb22ac160962deb22c65f193a5
schriftgestalt <georg.seifert@mac.com>
2017-02-04 15:00:54 +0100

    cleanup

--- PixelTool.glyphsTool/Contents/Resources/plugin.py
+++ PixelTool.glyphsTool/Contents/Resources/plugin.py
@@ -19,17 +19,14 @@ class PixelTool(SelectTool):
 	
 	def settings(self):
 		self.name = "Pixel Tool"
-		# self.generalContextMenus = [
-		# 	{'name': Glyphs.localize({'en': u'Layer info in Macro window', 'de': u'Ebenen-Infos in Makro-Fenster'}), 'action': self.printInfo},
-		# ]
 		self.keyboardShortcut = 'x'
-
+	
 	def start(self):
 		pass
-
+	
 	def activate(self):
 		pass
-
+	
 	def background(self, layer):
 		pass
 	
@@ -46,7 +43,6 @@ class PixelTool(SelectTool):
 			return
 		grid = font.grid
 		origin = NSPoint(math.floor(Loc.x / grid) * grid, math.floor(Loc.y / grid) * grid)
-		
 		
 		for c in layer.components:
 			if c.componentName == "pixel" and distance(c.position, origin) < 1:
@@ -75,43 +71,6 @@ class PixelTool(SelectTool):
 	def mouseUp_(self, event):
 		pass
 	
-	def conditionalContextMenus(self):
-		pass
-		# # Empty list of context menu items
-		# contextMenus = []
-		#
-		# # Execute only if layers are actually selected
-		# if Glyphs.font.selectedLayers:
-		# 	layer = Glyphs.font.selectedLayers[0]
-		#
-		# 	# Exactly one object is selected and it’s an anchor
-		# 	if len(layer.selection) == 1 and type(layer.selection[0]) == GSAnchor:
-		#
-		# 		# Add context menu item
-		# 		contextMenus.append({'name': Glyphs.localize({'en': u'Randomly move anchor', 'de': u'Anker zufällig verschieben'}), 'action': self.randomlyMoveAnchor})
-		#
-		# # Return list of context menu items
-		# return contextMenus
-
-	# def printInfo(self):
-	# 	"""
-	# 	Example for a method triggered by a context menu item.
-	# 	Fill in your own method name and code.
-	# 	Remove this method if you do not want any extra context menu items.
-	# 	"""
-	#
-	# 	# Execute only if layers are actually selected
-	# 	if Glyphs.font.selectedLayers:
-	# 		layer = Glyphs.font.selectedLayers[0]
-	#
-	# 		# Do stuff:
-	# 		print "Current layer:", layer.parent.name, layer.name
-	# 		print "  Number of paths:", len(layer.paths)
-	# 		print "  Number of components:", len(layer.components)
-	# 		print "  Number of anchors:", len(layer.anchors)
-
-		
-		
 	def __file__(self):
 		"""Please leave this method unchanged"""
 		return __file__
